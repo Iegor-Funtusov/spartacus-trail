a41973f657599b71228efd80c54a8fb2f6b8cd78
Iegor_Funtusov <iegor_funtusov@epam.com>
2021-01-14 14:14:05 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { translations, translationChunksConfig } from '@spartacus/assets';
+import { B2cStorefrontModule } from '@spartacus/storefront';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,32 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    B2cStorefrontModule.withConfig({
+      backend: {
+        occ: {
+          baseUrl: 'https://localhost:9002',
+          prefix: '/occ/v2/'
+        }
+      },
+      context: {
+        currency: ['USD'],
+        language: ['en'],
+        baseSite: ['electronics-spa']
+      },
+      authentication: {
+        client_id: 'spartacususer',
+        client_secret: 'password'
+      },
+      i18n: {
+        resources: translations,
+        chunks: translationChunksConfig,
+        fallbackLang: 'en'
+      },
+      features: {
+        level: '2.1'
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
